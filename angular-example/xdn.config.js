3288da7359c1a030b056a8f942e589e7511fc2bf
Tony Lepmets <tony.lepmets@moovweb.com>
2020-08-27 16:15:49 +0300

    Update Angular example
    XDN upgrade with some changes to generated code.

--- angular-example/xdn.config.js
+++ angular-example/xdn.config.js
@@ -1,13 +1,11 @@
-'use strict'
+"use strict";
 
 // This file was automatically added by xdn deploy.
 // You should commit this file to source control.
 
-const { join } = require('path')
-
 module.exports = {
   server: {
-    path: join(__dirname, 'dist/my-xdn-angular-app/server/main.js'),
-    export: 'app',
+    path: 'dist/my-xdn-angular-app/server/main.js',
+    export: 'app'
   },
 }
