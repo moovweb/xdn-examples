de7d0c72882ea84da7af34c7af5cdaacda01eb2f
Mark Brocato <mark.brocato@moovweb.com>
2020-09-21 23:46:12 +0300

    bundle server

--- sapper-example/webpack.config.js
+++ sapper-example/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
 		output: config.server.output(),
 		target: 'node',
 		resolve: { alias, extensions, mainFields },
-		externals: Object.keys(pkg.dependencies).concat('encoding'),
+		externals: ['encoding'],
 		module: {
 			rules: [
 				{
--- sapper-example/xdn.config.js
+++ sapper-example/xdn.config.js
@@ -1,5 +1,4 @@
 // This file was automatically added by xdn deploy.
 // You should commit this file to source control.
 module.exports = {
-  // includeNodeModules: true
 }
