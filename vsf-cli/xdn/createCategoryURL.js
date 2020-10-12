a32b6d573e8110f9198f45fd02926e2038b05dde
Richard van der Dys <richard.vanderdys@gmail.com>
2020-10-12 15:21:22 +0300

    Fixed category cache query URL

--- vsf-cli/xdn/createCategoryURL.js
+++ vsf-cli/xdn/createCategoryURL.js
@@ -1,18 +1,30 @@
 import { createApolloURL } from '@xdn/apollo'
 import { apolloClient, getSettings } from '@vue-storefront/commercetools-api'
-import { buildCategoryWhere } from '@vue-storefront/commercetools-api/src/helpers/search'
 import defaultQuery from '@vue-storefront/commercetools-api/src/api/getCategory/defaultQuery'
+
+const buildCategoryWhere = (search, acceptLanguage) => {
+  if (search?.catId) {
+    return `id="${search.catId}"`;
+  }
+
+  if (search?.slug) {
+    const predicate = acceptLanguage.map(locale => `${locale}="${search.slug}"`).join(' or ');
+    return `slug(${predicate})`;
+  }
+
+  return undefined;
+};
 
 const getVariables = (params) => {
   const { acceptLanguage } = getSettings();
   return params ? {
-    where: buildCategoryWhere(params),
+    where: buildCategoryWhere(params, acceptLanguage),
     limit: params.limit,
     offset: params.offset,
     acceptLanguage
   } : { acceptLanguage };
 }
 
-export default function createCategoryURL({id}) {
-  return createApolloURL(apolloClient, defaultQuery, getVariables({ id }))
+export default function createCategoryURL(category) {
+  return createApolloURL(apolloClient, defaultQuery, getVariables(category))
 }
