9517dc9bc2a72152e9207af5007c6e7a873d2bfd
kevhender <kevhender@gmail.com>
2020-11-19 17:27:48 -0500

    Initial Spartacus app

---
+++ spartacus-ex/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://aemspartacusapi.tmg.codes',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['electronics-spa'],
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.1'
+      }
+    }),
+    BrowserTransferStateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
